829492e668e253d74d781372d31a14a6780120cb
tdwright <tom@tdwright.co.uk>
2017-07-26 20:48:09 +0100

    Initial commit

---
+++ ProjectReference/Program.cs
@@ -0,0 +1,27 @@
+﻿using SharedLibrary;
+using System;
+
+namespace ProjectReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new SharedClass();
+
+            s.MagicNumber = 5;
+
+            var result = s.MultiplyMagicNumber(3);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+            SharedStruct s2;
+            s2.IntA = 1;
+            s2.IntB = 3;
+
+            SharedClass.ReverseToString<int>(2);
+        }
+    }
+}
---
+++ SharedLibrary/SharedClass.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+/// <summary>
+/// This library has all my shared stuff in.
+/// </summary>
+namespace SharedLibrary
+{
+    /// <summary>
+    /// Contains all the shared stuff
+    /// </summary>
+    public class SharedClass    
+    {
+        /// <summary>
+        /// Constructor for SharedClass
+        /// </summary>
+        public SharedClass()
+        {
+            MagicNumber = 1;
+        }
+
+        /// <summary>
+        /// Multiplies the MagicNumber by another
+        /// </summary>
+        /// <param name="Multiplier">The number to mutiply the MagicNumber by</param>
+        /// <returns>MagicNumber multiplied by Multiplier</returns>
+        public int MultiplyMagicNumber(int Multiplier)
+        {
+            return MagicNumber * Multiplier;
+        }
+
+        /// <summary>
+        /// Properties can have XML docs too!
+        /// </summary>
+        public int MagicNumber
+        {
+            get { return _magicNumber; }
+            set { _magicNumber = value; }
+        }
+        private int _magicNumber;
+        
+        /// <summary>
+        /// Generics get a special element: typeparam
+        /// </summary>
+        /// <typeparam name="T">The type parameter</typeparam>
+        /// <param name="obj">The first parameter</param>
+        /// <returns>Return value</returns>
+        public static string ReverseToString<T>(T obj)
+        {
+            var orig = obj.ToString();
+            var reversedCharArray = orig.ToCharArray().Reverse().ToArray();
+            return new string(reversedCharArray);
+        }
+    }
+
+    /// <summary>
+    /// Structs can have XML comments
+    /// </summary>
+    public struct SharedStruct
+    {
+        /// <summary>
+        /// Int A is best int
+        /// </summary>
+        public int IntA;
+
+        /// <summary>
+        /// Int B is good too
+        /// </summary>
+        public int IntB;
+    }
+}
